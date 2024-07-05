db7b02adcc55876aad1d4fddf5e7dbcc89ac02d1
Swayanshu Sekhar <swayanshupanda650@gmail.com>
2024-07-05 20:57:58 +0530

    first commit

--- index.js
+++ index.js
@@ -14,15 +14,6 @@ const wss = new WebSocketServer({ server });
 app.use(cors());
 app.use(express.json());
 
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    next();
-});
-
 let elements = [];
 let users = [];
 
